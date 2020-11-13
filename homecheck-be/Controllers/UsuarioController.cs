9cea0e06df5bfea80aa3f256b4e39af9a38eb4aa
Mariela  Bonilla <mbonilla.guti@gmail.com>
2020-11-13 02:24:00 -0600

    add be usuarios

---
+++ homecheck-be/Controllers/UsuarioController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using homecheck_be.Models;
+using homecheck_be.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace homecheck_be.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly UsuarioService _usuarioService;
+
+        public UsuarioController(UsuarioService usuarioService)
+        {
+            _usuarioService = usuarioService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Usuario>> Get() =>
+            _usuarioService.Get();
+
+
+        [HttpGet("{id:length(24)}", Name = "GetUsuario")]
+        public ActionResult<Usuario> Get(string id)
+        {
+            var usuario = _usuarioService.Get(id);
+
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            return usuario;
+        }
+
+        [HttpPost]
+        public ActionResult<Usuario> Create(Usuario usuario)
+        {
+            _usuarioService.Create(usuario);
+
+            return CreatedAtRoute("GetUsuario", new { id = usuario.Id.ToString() }, usuario);
+        }
+
+        [HttpPut("{id:length(24)}")]
+        public IActionResult Update(string id, Usuario usuarioIn)
+        {
+            var usuario = _usuarioService.Get(id);
+
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            _usuarioService.Update(id, usuarioIn);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:length(24)}")]
+        public IActionResult Delete(string id)
+        {
+            var usuario = _usuarioService.Get(id);
+
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            _usuarioService.Remove(usuario.Id);
+
+            return NoContent();
+        }
+
+    }
+}
---
+++ homecheck-be/Models/Usuario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace homecheck_be.Models
+{
+    public class Usuario
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string Email { get; set; }
+
+        public string Member { get; set; }
+
+        public string Usertype { get; set; }
+
+        public string IdFamilia { get; set; }
+    }
+}
