513f22f0135efb0457360588b741034d1ad56d1d
Eduardo Quiros <eduardo@eduardoquiros.com>
2020-12-18 01:56:32 -0600

    formatear service

--- homecheck-fe/ClientApp/src/app/servicios/usuario.service.ts
+++ homecheck-fe/ClientApp/src/app/servicios/usuario.service.ts
@@ -1,56 +1,60 @@
-import { Injectable } from '@angular/core';
-import { Inject } from '@angular/core';
-import { UsuarioList } from '../models/usuario-list';
-import { HttpClient } from '@angular/common/http';
-import { Usuario } from '../models/usuario';
-import { Observable } from 'rxjs';
+import { Injectable } from "@angular/core";
+import { Inject } from "@angular/core";
+import { UsuarioList } from "../models/usuario-list";
+import { HttpClient } from "@angular/common/http";
+import { Usuario } from "../models/usuario";
+import { Observable } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class UsuarioService {
   formData: UsuarioList;
   list: UsuarioList[];
   listUsuarios: Usuario[];
   private baseUrl: string;
-  private idFamilia = localStorage.getItem('id_familia');
+  private idFamilia = localStorage.getItem("id_familia");
 
-  constructor(
-    @Inject('BASE_URL') baseUrl: string,
-    private http: HttpClient) {
+  constructor(@Inject("BASE_URL") baseUrl: string, private http: HttpClient) {
     this.baseUrl = baseUrl;
   }
 
-
   postUsuario(formData: Usuario) {
-    return this.http.post(this.baseUrl + '/usuario/Nuevo', formData);
+    return this.http.post(this.baseUrl + "/usuario/Nuevo", formData);
   }
 
   putUsuario(id_user, id_familia: string, usuario: Usuario) {
-    return this.http.put(this.baseUrl + '/usuario/Update' + id_user, this.formData)
+    return this.http.put(
+      this.baseUrl + "/usuario/Update" + id_user,
+      this.formData
+    );
   }
 
   fillList() {
-    this.http.get(this.baseUrl + '/usuario/Get')
-      .toPromise().then(res => this.list = res as UsuarioList[])
+    this.http
+      .get(this.baseUrl + "/usuario/Get")
+      .toPromise()
+      .then((res) => (this.list = res as UsuarioList[]));
   }
 
   obtenerUsuariosFamilia(id_familia: string): Observable<UsuarioList[]> {
-    return this.http.get<UsuarioList[]>(this.baseUrl + '/usuario/UsuariosFamilia/' + id_familia);
+    return this.http.get<UsuarioList[]>(
+      this.baseUrl + "/usuario/UsuariosFamilia/" + id_familia
+    );
   }
 
   obtenerMimembrosFamilia(id_familia: string): Observable<UsuarioList[]> {
-    return this.http.get<UsuarioList[]>(this.baseUrl + '/usuario/MiembrosFamilia/' + id_familia);
+    return this.http.get<UsuarioList[]>(
+      this.baseUrl + "/usuario/MiembrosFamilia/" + id_familia
+    );
   }
 
   obtenerAdminFamilia(id_familia: string) {
-    let endpointUrl = this.baseUrl + '/usuario/AdminFamilia/' + id_familia;
+    let endpointUrl = this.baseUrl + "/usuario/AdminFamilia/" + id_familia;
     return this.http.get<Usuario>(endpointUrl);
   }
 
-
   deleteUsuario(id_familia, id_user: string) {
-    return this.http.delete(this.baseUrl + '/usuario/Delete' + id_user)
-
+    return this.http.delete(this.baseUrl + "/usuario/Delete" + id_user);
   }
 }
