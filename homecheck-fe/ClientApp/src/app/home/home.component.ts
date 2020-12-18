a8382045d263ea3a3cb90767e04573426b13db07
Eduardo Quiros <eduardo@eduardoquiros.com>
2020-12-18 01:02:21 -0600

    agregar init rudimentario a home component

--- homecheck-fe/ClientApp/src/app/home/home.component.ts
+++ homecheck-fe/ClientApp/src/app/home/home.component.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Usuario } from "../models/usuario";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { UsuarioService } from "../servicios/usuario.service";
+import { Router, ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-home',
-  styleUrls: ['./home.component.css'],
-  templateUrl: './home.component.html',
+  selector: "app-home",
+  styleUrls: ["./home.component.css"],
+  templateUrl: "./home.component.html",
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  private usuario: Usuario;
+  private loginForm: FormGroup;
+
+  constructor(
+    private usuarioService: UsuarioService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.loginForm = new FormGroup({
+      correo: new FormControl("", [Validators.required]),
+      password: new FormControl("", [Validators.required]),
+    });
+  }
 }
