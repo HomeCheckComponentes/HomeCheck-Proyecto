41adca46b2be8979d53ba6f619e3c842fbbfc76d
Eduardo Quiros <eduardo@eduardoquiros.com>
2020-12-19 00:27:24 -0600

    formatear

--- homecheck-fe/ClientApp/src/app/home/home.component.ts
+++ homecheck-fe/ClientApp/src/app/home/home.component.ts
@@ -28,41 +28,34 @@ export class HomeComponent implements OnInit {
     });
   }
 
-
   sanitizeData(data: FormGroup): Usuario {
     let userLogin: Usuario = new Usuario();
-    userLogin.email = this.loginForm.controls['correo'].value;
-    userLogin.password = this.loginForm.controls['password'].value;
+    userLogin.email = this.loginForm.controls["correo"].value;
+    userLogin.password = this.loginForm.controls["password"].value;
     return userLogin;
   }
-
-
 
   inicioSesion() {
     this.submitted = true;
     console.log(this.sanitizeData(this.loginForm));
 
-    this.usuarioService.obtenerUsuario(this.sanitizeData(this.loginForm))
+    this.usuarioService
+      .obtenerUsuario(this.sanitizeData(this.loginForm))
       .subscribe(
         (response) => {
-          sessionStorage.setItem('id_user', response['id'])
-          sessionStorage.setItem('id_familia', response['idFamilia'])
-          sessionStorage.setItem('username', response['username'])
-          sessionStorage.setItem('usertype', response['usertype'])
-          sessionStorage.setItem('member', response['member'])
-          sessionStorage.setItem('correo', response['email'])
-          this.usertype = response['usertype']
+          sessionStorage.setItem("id_user", response["id"]);
+          sessionStorage.setItem("id_familia", response["idFamilia"]);
+          sessionStorage.setItem("username", response["username"]);
+          sessionStorage.setItem("usertype", response["usertype"]);
+          sessionStorage.setItem("member", response["member"]);
+          sessionStorage.setItem("correo", response["email"]);
+          this.usertype = response["usertype"];
           this.validarTipoUsuario(this.usertype);
         },
         (error) => {
           console.log(error.error);
         }
-      )
-
-    
-
- 
-  
+      );
   }
 
   cerrarSesion() {
@@ -71,28 +64,31 @@ export class HomeComponent implements OnInit {
   }
 
   validarTipoUsuario(usertype: string) {
-
     switch (usertype) {
       case "1": //Admin total
-        this.router.navigate(['/familias/listar-familias']);
+        this.router.navigate(["/familias/listar-familias"]);
         break;
       case "2": //admin_familia
-        localStorage.setItem('id_familia', sessionStorage.getItem('id_familia'));
-        this.router.navigate(['/familias/listar-familias/perfil/' + sessionStorage.getItem('id_familia')]);
+        localStorage.setItem(
+          "id_familia",
+          sessionStorage.getItem("id_familia")
+        );
+        this.router.navigate([
+          "/familias/listar-familias/perfil/" +
+            sessionStorage.getItem("id_familia"),
+        ]);
         break;
       case "3": //usuario comun
-        localStorage.setItem('id_familia', sessionStorage.getItem('id_familia'));
-        this.router.navigate(['/tareas/registro-tareas']);
+        localStorage.setItem(
+          "id_familia",
+          sessionStorage.getItem("id_familia")
+        );
+        this.router.navigate(["/tareas/registro-tareas"]);
         break;
       default:
         sessionStorage.clear();
-        this.router.navigate['/home'];
+        this.router.navigate["/home"];
         break;
     }
-
   }
-
-
-
-
 }
